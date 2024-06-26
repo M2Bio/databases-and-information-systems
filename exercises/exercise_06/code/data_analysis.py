e6a05345476d5026a706a863fc532c432eb3ecfa
mariusrueve <44000950+mariusrueve@users.noreply.github.com>
2024-06-26 14:09:48 +0200

    Refactor data_analysis.py

--- exercises/exercise_06/code/data_analysis.py
+++ exercises/exercise_06/code/data_analysis.py
@@ -1,61 +1,59 @@
 from collections import defaultdict
-
 import psycopg2
 from tabulate import tabulate
 
 
 def analysis(geo, time, product):
-    if (
-        geo.lower() in ["name", "city", "region", "country"]
-        and time.lower() in ["wholedate", "day", "month", "quarter", "year"]
-        and product.lower() in ["name", "productgroup", "productfamily", "productcategory"]
-    ):
-        query = f"""
-            SELECT shop.{geo}, date.{time}, article.{product}, SUM(sold) AS total_sales
-            FROM fact_table
-            JOIN Shop ON fact_table.shopid = Shop.shopid
-            JOIN Article ON fact_table.articleid = Article.articleid
-            JOIN Date ON fact_table.wholedate = date.wholedate
-            GROUP BY CUBE (shop.{geo}, date.{time}, article.{product})
-            ORDER BY shop.{geo}, date.{time}, article.{product};
-            """
-        cur.execute(query)
-        all_data = cur.fetchall()
+    valid_geo = ["name", "city", "region", "country"]
+    valid_time = ["wholedate", "day", "month", "quarter", "year"]
+    valid_product = ["name", "productgroup", "productfamily", "productcategory"]
 
-        all_articles = set()
-        result = defaultdict(lambda: defaultdict(dict))
-        for d in all_data:
-            d = ["_Total" if v is None else v for v in d]
-            result[d[0]][d[1]][d[2]] = d[3]
-            all_articles.add(d[2])
-        # result: Ort -> Datum -> Produkt -> Anzahl
+    if geo.lower() not in valid_geo or time.lower() not in valid_time or product.lower() not in valid_product:
+        print("Invalid input.")
+        return
 
-        header = ["location", "sales"] + sorted(all_articles) + ["total"]
-        table_content = []
+    query = f"""
+        SELECT shop.{geo}, date.{time}, article.{product}, SUM(sold) AS total_sales
+        FROM fact_table
+        JOIN Shop ON fact_table.shopid = Shop.shopid
+        JOIN Article ON fact_table.articleid = Article.articleid
+        JOIN Date ON fact_table.wholedate = date.wholedate
+        GROUP BY CUBE (shop.{geo}, date.{time}, article.{product})
+        ORDER BY shop.{geo}, date.{time}, article.{product};
+        """
 
-        for store in result:
-            for day in result[store]:
-                new_table_row = [store, day]
-                for product in sorted(all_articles):
-                    product_count = result[store][day].get(product, 0)
-                    new_table_row.append(str(product_count))
-                table_content.append(new_table_row)
-
-        print(tabulate(table_content, headers=header, tablefmt="pretty"))
-
-
-if __name__ == "__main__":
-    conn = psycopg2.connect(
+    with psycopg2.connect(
         dbname="data_warehouse",
         user="postgres",
         password="postgres",
         host="localhost",
         port="5433",
-    )
-    cur = conn.cursor()
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            all_data = cur.fetchall()
 
+    all_articles = set()
+    result = defaultdict(lambda: defaultdict(dict))
+    for d in all_data:
+        d = ["_Total" if v is None else v for v in d]
+        result[d[0]][d[1]][d[2]] = d[3]
+        all_articles.add(d[2])
+
+    header = ["location", "sales"] + sorted(all_articles) + ["total"]
+    table_content = []
+
+    for store in result:
+        for day in result[store]:
+            new_table_row = [store, day]
+            for product in sorted(all_articles):
+                product_count = result[store][day].get(product, 0)
+                new_table_row.append(str(product_count))
+            table_content.append(new_table_row)
+
+    print(tabulate(table_content, headers=header, tablefmt="pretty"))
+
+
+if __name__ == "__main__":
     analysis("country", "quarter", "productFamily")
-
-    cur.close()
-    if conn is not None:
-        conn.close()
+    analysis("city", "year", "productCategory")
